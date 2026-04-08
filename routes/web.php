0f812486dee536868134c7f6b83ff203afa3a84a
andres <desarrollolibre.net@gmail.com>
2026-04-08 14:30:08 -0400

    pruebas locales ia

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,8 @@ use Laravel\Ai\Enums\Lab;
 use Maatwebsite\Excel\Facades\Excel;
 use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
+use Laravel\AI;
+
 use function Laravel\Ai\agent;
 
 Route::get('/', function () {
@@ -22,13 +24,43 @@ Route::get('/export-excel', function () {
 });
 Route::get('/laravel-ia-text', function () {
 
+    // $response = agent(
+    //     instructions: 'Eres un asistente experto en Laravel.',)->prompt(
+    //     'Genera una lista de 3 temas de Laravel 13 en formato JSON',
+    //     provider: Lab::Gemini,
+    //     model: 'gemini-2.0-flash',);
     $response = agent(
         instructions: 'Eres un asistente experto en Laravel.',)->prompt(
         'Genera una lista de 3 temas de Laravel 13 en formato JSON',
-        provider: Lab::Gemini,
-        model: 'gemini-2.0-flash',);
+         provider: 'openai', // ESTO ES VITAL
+         model: 'gemma-3-12b-it-IQ4_XS'
+        );
+
+    // $response = AI::chat('Explícame qué es un Repository Pattern en Laravel')
+    // ->model('llama3') // El nombre del modelo que tengas en local
+    // ->send();
     
     dd($response);
+});
+
+Route::get('/laravel-ia-text2', function () {
+    try {
+        $response = agent(
+            instructions: 'Eres un asistente experto en Laravel.',
+        )->prompt(
+            'Genera una lista de 3 temas de Laravel 13 en formato JSON',
+            provider: 'openai',
+            model: 'gemma-3-12b-it-IQ4_XS' // Copiado exactamente de tu JSON
+        );
+
+        dd($response);
+    } catch (\Exception $e) {
+        // Esto nos dirá qué está pasando realmente si falla
+        return response()->json([
+            'message' => $e->getMessage(),
+            'check_jan' => 'Asegúrate de que el servidor en Jan.ia esté en "Started"'
+        ], 500);
+    }
 });
 
 
